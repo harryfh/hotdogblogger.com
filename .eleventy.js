a8922fd0340d7f8ae86777d69b9fa6db2761679c
Harry <harryfinkhynes@gmail.com>
2024-04-18 16:00:29 -0400

    Added taildwind and navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/style.css")
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
 
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/main.css")
   return {
     dir: {
       input: "src",
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/_includes/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
