824fb20936c787a5e0cd0ede42694f6fc7e817c2
Harry <harryfinkhynes@gmail.com>
2024-04-18 22:10:56 -0400

    hot dog man

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,12 @@ module.exports = {
     "./src/_includes/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
     "./src/_includes/base.njk",
   ],
+  theme: {
+    colors: {
+      "yellow": "#ffd966",
+      "red": "#990000",
+      "light-gray": "#eee",
+      "dark-gray": "#333",
+    },
+  },
 }
