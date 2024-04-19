458669189a1cec04d7ef98a954d8fb844d487f4b
Harry <harryfinkhynes@gmail.com>
2024-04-19 14:06:15 -0400

    Lots of style changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,18 @@ module.exports = {
     "./src/_includes/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
     "./src/_includes/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
     "./src/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
+    "./src/**/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
     "./src/_includes/base.njk",
   ],
   theme: {
     colors: {
       "yellow": "#ffd966",
-      "red": "#990000",
-      "red-2": "#ff3333",
+      "red": "#AA0000",
+      "red-2": "#BB1411",
       "light-gray": "#eee",
       "dark-gray": "#222",
-      "brown": "#C4A484",
+      "brown-1": "#ffd08a",
+      "brown-2": "#f26635",
     },
   },
 }
